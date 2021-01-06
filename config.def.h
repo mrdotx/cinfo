13114db23c1fb50914e9f9670510b000ff6b2341
mrdotx <klassiker@gmx.de>
2021-01-06 17:29:49 +0100

    header constants removed and color constants added

--- config.def.h
+++ config.def.h
@@ -2,37 +2,36 @@
  * path:       /home/klassiker/.local/share/repos/cinfo/config.def.h
  * author:     klassiker [mrdotx]
  * github:     https://github.com/mrdotx/cinfo
- * date:       2021-01-05T12:21:08+0100
+ * date:       2021-01-06T17:24:53+0100
  */
 
 /* package manager */
-const char *PKGCMD = "pacman -Qq | wc -l",
-           *PKGDESC = " [pacman]";
-
-/* header */
-const char *HEADERSPACER = " ",
-           *HEADERBINDER = "@";
+const char *PKG_CMD = "pacman -Qq | wc -l",
+           *PKG_DESC = " [pacman]";
 
 /* ascii version */
-const int ASCIILINELEFTLEN = 8;
-const char *ASCIILINE = "-",
-           *ASCIILINEDIVIDER = "+",
-           *ASCIIDIVIDER = " | ";
+const int ASCII_LINELEFTLEN = 8;
+const char *ASCII_LINE = "-",
+           *ASCII_LINEDIVIDER = "+",
+           *ASCII_DIVIDER = " | ";
 
 /* color version */
-const int COLORLINELEFTLEN = 13;
-const char *COLORLINE = "─",
-           *COLORLINEDIVIDERTOP = "┬",
-           *COLORLINEDIVIDERBOTTOM = "┴",
-           *COLORDIVIDER = " │ ",
-           *COLORSYMBOL = "██";
+const int COLOR_LINELEFTLEN = 13;
+const char *COLOR_PRIMARY = "\033[1;37m",
+           *COLOR_SECONDARY = "\033[0;37m",
+           *COLOR_TABLE = "\033[1;34m",
+           *COLOR_LINE = "─",
+           *COLOR_LINEDIVIDERTOP = "┬",
+           *COLOR_LINEDIVIDERBOTTOM = "┴",
+           *COLOR_DIVIDER = " │ ",
+           *COLOR_SYMBOL = "██";
 
 /* label */
-const char *DISTROTXT = " distro",
-           *MODELTXT  = "  model",
-           *KERNELTXT = " kernel",
-           *UPTIMETXT = " uptime",
-           *PKGSTXT   = "   pkgs",
-           *SHELLTXT  = "  shell",
-           *CPUTXT    = "    cpu",
-           *RAMTXT    = "    ram";
+const char *DISTRO_TXT = " distro",
+           *MODEL_TXT  = "  model",
+           *KERNEL_TXT = " kernel",
+           *UPTIME_TXT = " uptime",
+           *PKGS_TXT   = "   pkgs",
+           *SHELL_TXT  = "  shell",
+           *CPU_TXT    = "    cpu",
+           *RAM_TXT    = "    ram";
