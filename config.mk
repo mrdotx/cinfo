5b1630ff7dc2ae21c683dc7ef7af6a540824a744
mrdotx <klassiker@gmx.de>
2021-05-22 13:01:51 +0200

    version bump to 0.3.5

--- config.mk
+++ config.mk
@@ -1,10 +1,10 @@
 # path:   /home/klassiker/.local/share/repos/cinfo/config.mk
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/cinfo
-# date:   2021-05-20T10:15:17+0200
+# date:   2021-05-22T12:57:24+0200
 
 # cinfo version
-VERSION = 0.3.4
+VERSION = 0.3.5
 
 # paths
 PREFIX	= /usr/local
