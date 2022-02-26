4d90f9fb97e0fb64e491f4448b775a2cc937065e
mrdotx <klassiker@gmx.de>
2022-02-26 08:57:27 +0100

    name added

--- config.mk
+++ config.mk
@@ -1,15 +1,16 @@
 # path:   /home/klassiker/.local/share/repos/cinfo/config.mk
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/cinfo
-# date:   2022-01-24T17:38:35+0100
+# date:   2022-02-25T20:43:32+0100
 
-# cinfo version
+# tool info
+NAME = cinfo
 VERSION = 0.4.0
 
 # paths
-PREFIX	= /usr/local
-BINDIR	= $(PREFIX)/bin
-MANDIR	= $(PREFIX)/share/man
+PREFIX = /usr/local
+BINDIR = $(PREFIX)/bin
+MANDIR = $(PREFIX)/share/man
 
 CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE
 # if your system is not POSIX, add -std=c99 to CFLAGS
