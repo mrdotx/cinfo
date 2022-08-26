9190a6f963f119a7244412863a824180b87e829b
Jonathan Wright <jonathan@effecthost.com>
2022-08-26 12:03:23 -0500

    Use gcc -O2
    optimizations > size

--- config.mk
+++ config.mk
@@ -14,7 +14,7 @@ MANDIR ?= $(PREFIX)/share/man
 
 CPPFLAGS += -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE
 # if your system is not POSIX, add -std=c99 to CFLAGS
-CFLAGS += -Os
+CFLAGS += -O2
 LDFLAGS ?= -s -lpthread
 
 # compiler and linker
