e2b570c7ddda1526ed7e3be52eb9b07cbfd6f816
mrdotx <klassiker@gmx.de>
2024-02-05 09:10:50 +0100

    version changed to 0.5.5

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
 # path:   /home/klassiker/.local/share/repos/cinfo/config.mk
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/cinfo
-# date:   2023-09-03T09:34:08+0200
+# date:   2024-02-05T09:10:39+0100
 
 # tool info
 NAME = cinfo
-VERSION = 0.5.4
+VERSION = 0.5.5
 
 # paths
 PREFIX ?= /usr/local
